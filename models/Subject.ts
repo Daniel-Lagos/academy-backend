c8ef7cb9f22b3820b01c373be430bdf733adff38
Daniel Lagos <daniel.lagos010@gmail.com>
2021-08-20 16:20:01 -0500

    create subject schema

---
+++ models/Subject.ts
@@ -0,0 +1,21 @@
+import { Document, model, Schema } from 'mongoose';
+
+interface ISubject extends Document {
+  id: string,
+  name: string,
+}
+
+const SubjectSchema: Schema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+});
+
+SubjectSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+module.exports = model<ISubject>('subject', SubjectSchema);
